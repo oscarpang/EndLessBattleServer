7ae83f452d6286da7d1b1f69cec5d77308636fb8
weiwupan <weiwupan@usc.edu>
2015-11-18 16:49:30 -0800

    Fix error

--- Dump20151118.sql
+++ Dump20151118.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `LoginInfo`
 --
 
+CREATE DATABASE  IF NOT EXISTS `EndLessBattle`;
+
 DROP TABLE IF EXISTS `LoginInfo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
